bc8e48483fc192e0f5667114ab3c3700bec50644
“graciegould” <graciegould5@gmail.com>
2024-10-16 15:24:45 -0700

    allow users to create project in current folder, allow users to overwrite existing projects

--- cli.js
+++ cli.js
@@ -3,28 +3,80 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
+const clc = require('cli-color'); 
+const { setInterval, clearInterval } = require('timers');
 
-const projectName = process.argv[2];
-if (!projectName) {
-  console.error('Please specify the project directory:');
-  console.log('  npx create-dynamic-server <project-directory>');
-  process.exit(1);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const checkExistingNodeProject = (projectPath) => {
+  return fs.existsSync(path.join(projectPath, 'package.json'));
+};
+
+const promptUser = (question) => {
+    return new Promise((resolve) => {
+    rl.question(clc.yellow(question), (answer) => {
+      resolve(answer.toLowerCase());
+    });
+  });
+};
+
+const startSpinner = (message) => {
+  const spinnerChars = ['|', '/', '-', '\\'];
+  let i = 0;
+
+  const interval = setInterval(() => {
+    process.stdout.write('\r' + message + clc.cyan(spinnerChars[i++ % spinnerChars.length]) + ' ');
+  }, 100);
+
+  return () => {
+    clearInterval(interval);
+    process.stdout.write('\r' + message + clc.green('✔') + '\n'); 
+  };
+};
+
+let projectName = process.argv[2] || '.';
+let projectPath = '';
+
+if (projectName === '.') {
+  projectPath = process.cwd();
+  projectName = path.basename(projectPath);
+} else {
+  projectPath = path.resolve(process.cwd(), projectName);
 }
 
-const projectPath = path.resolve(process.cwd(), projectName);
 const repoURL = 'https://github.com/graciegould/super-dynamic-server.git';
 
-try {
-  console.log(`Cloning repository into ${projectPath}...`);
-  execSync(`git clone ${repoURL} "${projectPath}" --depth 1`);
-  process.chdir(projectPath);
-  fs.rmSync(path.join(projectPath, '.git'), { recursive: true, force: true });
-  console.log('Installing dependencies...');
-  execSync('npm install', { stdio: 'inherit' });
-  console.log('Setup complete! To get started:');
-  console.log(`  cd ${projectName}`);
-  console.log('  npm start');
-} catch (error) {
-  console.error('An error occurred:', error.message);
-  process.exit(1);
-}
+(async () => {
+  try {
+    if (checkExistingNodeProject(projectPath)) {
+      const answer = await promptUser('A Node.js project already exists in this directory. Do you want to overwrite it? (y/n): ');
+
+      if (answer !== 'y') {
+        console.log(clc.red('Project creation aborted.'));
+        process.exit(0);
+      }
+    }
+    const stopCloneSpinner = startSpinner(`Cloning repository into ${clc.green(projectPath)}...`);
+    execSync(`git clone ${repoURL} "${projectPath}" --depth 1`);
+    stopCloneSpinner();  
+    process.chdir(projectPath);
+    const stopRemoveSpinner = startSpinner('Removing .git folder...');
+    fs.rmSync(path.join(projectPath, '.git'), { recursive: true, force: true });
+    stopRemoveSpinner();
+    const stopInstallSpinner = startSpinner('Installing dependencies...');
+    execSync('npm install', { stdio: 'inherit' });
+    stopInstallSpinner();
+    console.log(clc.cyan('\nSetup complete! To get started:'));
+    console.log(clc.yellow(`  cd ${projectName}`));
+    console.log(clc.yellow('  npm start'));
+  } catch (error) {
+    console.error(clc.red('An error occurred:', error.message));
+    process.exit(1);
+  } finally {
+    rl.close();
+  }
+})();
